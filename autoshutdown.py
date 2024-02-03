baffe0fe04a35738a4745dfe29f479cdec81be7a
Jahdai Cintron <jahdaic@yahoo.com>
2024-02-02 20:10:06 -0500

    Added First Version of Script

---
+++ autoshutdown.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os
+import time
+from pisugar import *
+
+conn, event_conn = connect_tcp('pizero2.local')
+pi_sugar_server = PiSugarServer(conn, event_conn)
+disconnected_time = 0;
+
+while True:
+	power = pi_sugar_server.get_battery_power_plugged()
+
+	if power and disconnected_time:
+		disconnected_time = 0
+		print('Power Reconnected')
+
+	if not power and not disconnected_time:
+		disconnected_time = time.time()
+		print('Power Disconnected')
+
+	if disconnected_time and time.time() - disconnected_time >= 30:
+		os.system('sudo shutdown now')
+
+	time.sleep(1)
